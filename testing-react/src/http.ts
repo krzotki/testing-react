58b6b99ad910e87459b16c75db9f0bf2461798d2
krzotki <krzotki@gmail.com>
2021-08-08 18:35:59 +0200

    added profile page

--- testing-react/src/http.ts
+++ testing-react/src/http.ts
@@ -1,3 +1,5 @@
 import axios from "axios";
 
 export const myRequest = axios.get("https://jsonplaceholder.typicode.com/posts").then(response => response.data);
+
+export const usersRequest = axios.get(`https://jsonplaceholder.typicode.com/users`).then(response => response.data);
--- testing-react/src/store/AppContext.tsx
+++ testing-react/src/store/AppContext.tsx
@@ -1,30 +1,48 @@
 
 import React, {useState, useEffect} from "react";
 import Post from '../models/Post';
+import User from '../models/User';
 
-import { myRequest } from './../http';
+import { myRequest, usersRequest } from './../http';
 
 interface AppContextObj {
     posts: Post[],
+    users: User[],
+    getUser: (userId: number) => User | undefined
 }
 
 export const AppContext = React.createContext<AppContextObj>({
-    posts: []
+    posts: [],
+    users: [],
+    getUser: (userId: number) => undefined,
 })
 
 
 const AppContextProvider: React.FC = (props) => {
     const [posts, setPosts] = useState<Post[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
 
+    const getUserHandler = (userId: number) => {
+        return users.find(user => user.id === userId);
+    };
+    
     useEffect(() => {
         myRequest.then(data => {
             console.log(data)
             setPosts(data)
         });
+
+        usersRequest.then(data => {
+            console.log(data);
+            setUsers(data);
+        });
+
     }, []);
     
     const contextValue: AppContextObj = {
-        posts: posts
+        posts: posts,
+        users: users,
+        getUser: getUserHandler
     };
 
     return <AppContext.Provider value={contextValue}>{props.children}</AppContext.Provider>
